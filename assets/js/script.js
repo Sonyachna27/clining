1b2688a771edd36ecd5d688d78cc108b4c7dbe82
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-12-29 22:24:28 +0200

    add section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,48 @@
 
-// $('.beforeAfter').beforeAfter({
+document.addEventListener("DOMContentLoaded", function () {
+	
+	toggleMenu();
+	animationHeader();
+});
 
-//   // is draggable/swipeable
-//   movable: true,
+const animationHeader = () =>{
+	let lastScrollTop = 0;
 
-//   // click image to move the slider
-//   clickMove: true,
+  window.addEventListener("scroll", function () {
+		const headerNav = document.querySelector(".header__bottom");
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		let windowInnerWidth = window.innerWidth;
+    if (windowInnerWidth >= 1024) {
+      if (scrollTop > lastScrollTop) {
+        if (scrollTop > 100) {
+          headerNav.classList.add("fixed-header-nav");
+          headerNav.style.animationName = "smoothScroll";
+        }
+      } else if (scrollTop <= 0) {
+        headerNav.classList.remove("fixed-header-nav");
+        headerNav.style.animationName = "removeSmoothScroll";
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }
+  });
+}
+const toggleMenu = () =>{
+	const htmlElement = document.querySelector("html");
+	const burgerMenu = document.querySelector(".burger");
+  const navLinks = document.querySelectorAll("nav a");
+  burgerMenu.addEventListener("click", () =>
+    htmlElement.classList.toggle("open")
+  );
 
-//   // always show the slider
-//   alwaysShow: true,
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      htmlElement.classList.remove("open");
+    });
+  });
+}
 
-//   // intial position of the slider
-//   position: 50,
-
-//   // opacity between 0 and 1
-//   opacity: 0.4,
-//   activeOpacity: 1,
-//   hoverOpacity: 0.8,
-
-//   // slider colors
-//   separatorColor: '#ffffff',
-//   bulletColor: '#ffffff',
-//   arrowColor: '#333333',
-  
-// });
-
-$('.ba-slider').beforeAfter();
+$(document).ready(function () {
+  $('.ba-slider').each(function () {
+    $(this).beforeAfter();
+  });
+});
